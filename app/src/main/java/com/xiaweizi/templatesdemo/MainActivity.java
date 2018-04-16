b7c232cce89451e308f585e369e2db45b4752994
xiaweizi <1012126908@qq.com>
2018-04-16 14:20:37 +0800

    [dev] 初次提交

---
+++ app/src/main/java/com/xiaweizi/templatesdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xiaweizi.templatesdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
